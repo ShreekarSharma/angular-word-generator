b6d58ab6f7571f61f155c357b045efec48522e29
Shreekar Sharma <shreekar@mail.com>
2024-01-26 16:02:43 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import arrayWords from "../utils/words"
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  countriesArray: string[] = [
+    'United States',
+    'Canada',
+    'United Kingdom',
+    'Germany',
+    'France',
+    'Japan',
+    'Australia',
+    'India',
+    'Brazil',
+    'South Africa',
+  ];
   title = 'word-generator';
+  words = '';
+  countries = '';
+  limit = 10;
+  num = 0;
+  handleSlideChange(newLimit: Event) {
+    let htmlElement = newLimit.target as HTMLInputElement;
+    this.limit = parseInt(htmlElement.value);
+  }
+  generate() {
+    this.words = arrayWords.slice(0, this.limit).join(' ');
+  }
 }
